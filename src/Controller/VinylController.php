92c8094593590a42572d99351a8600ef89645e90
Brandon Dautzenberg <brandondautzenberg@hotmail.com>
2023-12-14 10:06:05 +0100

    Added twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,15 +4,24 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Holy Wars', 'artist' => 'Megadeth'],
+            ['song' => 'Powerslave', 'artist' => 'Iron Maiden'],
+            ['song' => 'Ace of Spades', 'artist' => 'Motörhead'],
+            ['song' => 'Scorpio Curse', 'artist' => 'Electric Wizard'],
+            ['song' => 'Pillars of Sand', 'artist' => 'Vektor'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', ['title' => 'PB & Jams', 'tracks' => $tracks,]);
     }
 
     #[Route('/browse/{slug}')]
